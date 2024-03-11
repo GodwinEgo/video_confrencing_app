6a699c4f68a37a18028982640f8006eae3815b80
GodwinEgo <godwinehile04@gmail.com>
2024-03-11 20:25:38 +0100

    feat: average number

--- first_program.rb
+++ first_program.rb
@@ -1,4 +1,34 @@
-a = b = 5
-puts a
-puts b
-puts a === b
+def calculate_average
+  sum = 0.0
+  count = 0
+
+  loop do
+    puts "Enter a number (or 'exit' to finish):"
+    input = gets.chomp
+
+    if input.downcase == 'exit'
+      break
+    end
+
+    unless valid_number?(input)
+      puts "Invalid input. Please enter a number."
+      break
+    end
+
+    sum += input.to_f
+    count += 1
+  end
+
+  if count > 0
+    average = sum / count
+    puts "The average is #{average}"
+  else
+    puts "No numbers were entered."
+  end
+end
+
+def valid_number?(str)
+  Float(str) != nil rescue false
+end
+
+calculate_average
